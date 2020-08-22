f41a6a19d2dcd5938a2b5d8e4f611a6c3ea21ebd
andreiyusupau <nevermind.qqq@gmail.com>
2020-08-22 12:13:47 +0300

    Create domain layer.

---
+++ src/main/java/com/nevermind/domain/Status.java
@@ -0,0 +1,5 @@
+package com.nevermind.domain;
+
+public enum Status {
+    ACTIVE, INACTIVE
+}
---
+++ src/main/java/com/nevermind/domain/UserAccount.java
@@ -0,0 +1,82 @@
+package com.nevermind.domain;
+
+import java.time.LocalDate;
+
+public class UserAccount {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Role role;
+    private Status status;
+    private LocalDate createdAt;
+
+    public UserAccount() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
