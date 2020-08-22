bf37a17f806bd728632d01c990899c23300ec9cc
andreiyusupau <nevermind.qqq@gmail.com>
2020-08-22 13:22:09 +0300

    Create core classes.

---
+++ src/main/java/com/nevermind/usercontrolservice/UserControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nevermind.usercontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserControlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserControlServiceApplication.class, args);
+    }
+
+}
