faa67e207b1fe20bbe2fe8125b7086c8aa839c4c
andreiyusupau <nevermind.qqq@gmail.com>
2020-08-23 15:28:32 +0300

    Add authorization.

--- src/main/java/com/nevermind/usercontrolservice/controller/UserController.java
+++ src/main/java/com/nevermind/usercontrolservice/controller/UserController.java
@@ -3,6 +3,7 @@ package com.nevermind.usercontrolservice.controller;
 import com.nevermind.usercontrolservice.domain.Role;
 import com.nevermind.usercontrolservice.domain.UserAccount;
 import com.nevermind.usercontrolservice.service.UserAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -42,12 +43,14 @@ public class UserController {
         return "view";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping(path="/user/new")
     public String createUserAccount(Model model) {
         model.addAttribute("userAccount",new UserAccount());
         return "new";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping(path="/user/new")
     public @ResponseBody String addUserAccount(@Valid @ModelAttribute UserAccount userAccount, BindingResult bindingResult) {
     if (bindingResult.hasErrors()){
@@ -71,6 +74,7 @@ public class UserController {
         return "Saved";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping(path="/user/{id}/edit")
     public String editUserAccount(@PathVariable Long id, Model model) {
         Optional<UserAccount> userAccount= userAccountService.findById(id);
@@ -78,6 +82,7 @@ public class UserController {
         return "edit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping(path="/user/{id}/edit")
     public @ResponseBody String updateUserAccount(@PathVariable Long id,@ModelAttribute UserAccount userAccount) {
         userAccountService.save(userAccount);
